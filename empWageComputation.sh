3ea9a1ee330bc51c61c7bebb53c8f043a5e0fb8e
avinash7773 <javinash228@gmail.com>
2021-02-24 09:28:36 +0530

    UC-4 use Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -x
 
 echo "Wel-Come"
+echo "Use Case Statement"
 
 ratePerHour=20
 random=$((RANDOM%3))
-if(($random==0))
-then
-	dailyHour=0
-	echo "employee is absent"
-elif(($random==2))
-then
-	dailyHour=8
-	echo "employee is fullTime and salery ="
-else
-	dailyHour=4
-	echo "employee is  PartTime and salery="
-fi
+
+case $random in
+	0) dailyHour=0
+	   echo "employee is absent"
+	;;
+	2) dailyHour=8
+	   echo "employee is fullTime and salery ="
+	;;
+	1) dailyHour=4
+	   echo "employee is  PartTime and salery="
+	;;
+esac
 
 salery=$(($random*$dailyHour*$ratePerHour))
 echo $salery

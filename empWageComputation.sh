fe10414c76b03339de66693917f2839c20020d01
avinash7773 <javinash228@gmail.com>
2021-02-24 09:07:38 +0530

    UC-2 calculateEmpDailyWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,11 +2,14 @@
 
 echo "Wel-Come"
 
+ratePerHour=20
+dailyHour=8
 random=$((RANDOM%2))
 if(($random==0))
 then
 	echo "employee is absent"
 else
-	echo "employee is present"
+	salery=$(( $random*$dailyHour*$ratePerHour ))
+	echo "employee is present and salery =" $salery
 fi
 

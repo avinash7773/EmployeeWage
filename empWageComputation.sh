3153d520d736ab854e939dcc083ef266b3f105af
avinash7773 <javinash228@gmail.com>
2021-02-24 10:02:48 +0530

    UC-5 calculateWage for month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,20 +4,18 @@ echo "Wel-Come"
 echo "Use Case Statement"
 
 ratePerHour=20
-random=$((RANDOM%3))
 
-case $random in
-	0) dailyHour=0
-	   echo "employee is absent"
-	;;
-	2) dailyHour=8
-	   echo "employee is fullTime and salery ="
-	;;
-	1) dailyHour=4
-	   echo "employee is  PartTime and salery="
-	;;
-esac
-
-salery=$(($random*$dailyHour*$ratePerHour))
-echo $salery
-
+TotalSalery=0
+workingDay=1
+for((day=1;day<=30;day++))
+do
+	while(($workingDay<=20))
+	do
+		dailyHour=8
+		(( workingDay++ ))
+		salery=$(($ratePerHour*$dailyHour))
+		TotalSalery=$(($salery+$TotalSalery))
+		(( $workingDay++ ))
+	done
+done
+echo $TotalSalery
